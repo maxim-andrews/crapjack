652fe1f362de3cb09abfd3da75394adba54d738f
Maxim Andrews <maximandrewz@users.noreply.github.com>
2018-11-04 03:51:58 +0000

    add service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import renderRoot from './containers/index.switch';
 import './index.scss';
 
 renderRoot();
-// registerServiceWorker();
+registerServiceWorker();
