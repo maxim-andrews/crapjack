31999691d812c50779dc6332570ac440b839d29f
Maxim Andrews <maximandrewz@users.noreply.github.com>
2018-12-29 08:08:10 +0000

    fix reducers dev reload

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -29,9 +29,10 @@ export default function configureStore(initialState) {
 
   // Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
   if (module.hot) {
-    module.hot.accept('../reducers', () =>
-      store.replaceReducer(createRootReducer(history))
-    );
+    module.hot.accept('../reducers', () => {
+      const createRootReducer = require('../reducers').default;
+      store.replaceReducer(createRootReducer(history));
+    });
   }
 
   return { history, store };
